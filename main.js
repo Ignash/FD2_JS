ed0ac4ef68e432cde38c6bc61ec5adabd5213184
Maksim Ihnashevich <maksimignashevich@gmail.com>
2018-06-11 21:03:40 +0300

    add

---
+++ main.js
@@ -0,0 +1,54 @@
+const routeConfig = {
+    home: 'home',
+    info: 'info',
+    user: {
+        list: 'user-list',
+        edit: 'edit-user',
+        create: 'create-user'
+    }
+},
+notFoundPageTemplateId = 'page-not-found',
+routedElement = $('main');
+
+onHashChange();
+$(window).on('hashchange', onHashChange);
+
+function onHashChange() {
+const path = getHashPath(),
+    templateId = getTemplateId(path, routeConfig) || notFoundPageTemplateId;
+
+setTemplate(routedElement, templateId);
+}
+
+function getTemplateId(path, route) {
+        if (path.length != 0) {
+            route = route[path[0]];
+            if (typeof route !== "undefined"){
+                path.shift();
+                return getTemplateId(path, route);
+            }else {
+                return undefined;
+            }
+        } else{
+            return route;
+        }
+    }
+    // for (var i=0; i <path.length; i++) {
+    //     route = route[path[i]];
+    // }
+
+        // return route;
+    
+
+function getHashPath() {
+return location.hash.slice(1).split('/');
+}
+
+function setTemplate(destinationElement, templateId) {
+const temlateElement = document.getElementById(templateId);
+
+var clone = document.importNode(temlateElement.content, true);
+
+destinationElement.empty();
+destinationElement.append(clone);
+}

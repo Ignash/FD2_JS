9068f59ec3cd113136f296231438441182785d92
Maksim Ihnashevich <maksimignashevich@gmail.com>
2018-07-10 11:26:59 +0300

    Object.prototyp

--- main.spec.js
+++ main.spec.js
@@ -1,221 +1,336 @@
-describe('Дата и время', function() {
+describe('ООП в прототипном чтиле', function() {
+    describe('Object prototype', function () {
 
-    it ('Создание даты', function(){
+        it ('prototype __proto__', function(){
+            var human = {
+                    eats:true
+                },
+                man = {
+                    running:true
+                };  
+            man.__proto__ = human;
+    
+            expect(man.running).toBeTruthy();
+            expect(man.eats).toBeTruthy();
 
-        var day = new Date();
-        expect(typeof day).toBe('object');
-
-    });
-
-    it('Вывод даты', function(){
-
-        var day = new Date(2011, 10, 12, 23, 45);
-
-        var DayString = day.toString();
-        expect(DayString).toEqual("Sat Nov 12 2011 23:45:00 GMT+0300 (Саудовская Аравия, стандартное время)");
-
-        var DateString = day.toDateString();
-        expect(DateString).toEqual("Sat Nov 12 2011");
-
-        var TimeString = day.toTimeString();
-        expect(TimeString).toEqual('23:45:00 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-        var option = {
-            era: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            weekday: 'long',
-            timezone: 'UTC',
-            hour: undefined,
-            minute: undefined,
-            second: undefined,
-            timeZoneName:'short'
+            man.eats = false;
+            expect(man.eats).toBeFalsy();
+            expect(human.eats).toBeTruthy();
+            
+            man = {
+                running:true,
+                __proto__ : human
             };
+            expect(man.running).toBeTruthy();
+            expect(man.eats).toBeTruthy();
+        });
         
-        
-        expect(day.toLocaleString('ru', option)).toEqual('суббота, 12 ноября 2011 г. от Рождества Христова, GMT+3');
-        
+        it ('hasOwnProperty', function (){
 
-    });
+            var human = {
+                    eats:true
+                },
+                man = {
+                    running:true,
+                    __proto__ : human
+                };
 
-    describe ('Компоненты даты', function(){
+            expect(man.hasOwnProperty('running')).toBeTruthy();
+            expect(man.hasOwnProperty('eats')).toBeFalsy();
 
-        var myDay = new Date(2011, 9, 30, 2, 30, 50, 320);
+            spy = jasmine.createSpy('spy');
 
-        it('Год', function(){
-            var year = myDay.getFullYear();
-            expect(year).toBe(2011);
+            for (var key in man) {
+                if (!man.hasOwnProperty(key)) {
+                    continue;
+                } else {
+                    spy(key);
+                }        
+            }
+
+            expect(spy.calls.count()).toBe(1);
+            expect(spy).toHaveBeenCalledWith('running');
         });
 
-        it('Месяц', function(){
-
-            var myMonth = myDay.getMonth();
-            expect(myMonth).toBe(9);
-    
-            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-            expect(months[myMonth]).toBe('October');
-
-        });
-
-        it('Дата', function(){
-
-            var date = myDay.getDate();
-            expect(date).toBe(30);
+        it ('Object.create()', function () {
+            var human = {
+                    speak:'Hello!'
+                },
+                man = Object.create(null);
             
-        });
+            man.speak = 'Hello!';
 
-        it('Час', function(){
-
-            var hours = myDay.getHours();
-            expect(hours).toBe(2);
+            expect(human.toString).toBeDefined();
+            expect(man.toString).toBeUndefined();
 
         });
         
-        it('Минуты', function(){
+        it ('Object.getPrototypeOf()', function () {
+            var human = {
+                    eats:true
+                },
+                man = {
+                    running:true
+                };  
+            man.__proto__ = human;
 
-            var minutes = myDay.getMinutes();
-            expect(minutes).toBe(30)
-
+            expect(Object.getPrototypeOf(man)).toBe(human)
         });
 
-        it('Секунды', function(){
+        it ('Object.setPrototypeOf()', function (){
+            var human = {
+                    eats:true
+                },
+                man = {
+                    running:true
+                };
 
-            var seconds = myDay.getSeconds();
-            expect(seconds).toBe(50)
+            Object.setPrototypeOf(man, human);
+            expect(Object.getPrototypeOf(man)).toBe(human);
 
-        });
-
-        it('Миллисекунды', function(){
-
-            var milliseconds = myDay.getMilliseconds();
-            expect(milliseconds).toBe(320)
-
-        });
-        
-        it('День', function(){
-
-            var day = myDay.getDay();
-            expect(day).toBe(0);
-
-            var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            expect(days[day]).toBe('Sunday');
-
-        });
-
-        it('Число миллисекунд с 1 января 1970года ', function(){
-
-            var time = myDay.getTime();
-            expect(time).toBe(1319931050320);
-
-        });
-
-        it('Разница между указанным временем и UTC-временем, в минутах', function(){
-
-            var time = myDay.getTimezoneOffset();
-            expect(time).toBe(-180);
-
+            Object.setPrototypeOf(man, null);
+            expect(Object.getPrototypeOf(man)).toBeNull();
         });
     });
-    describe('Установка компонентов', function(){
 
+    describe ('Object.prototype, new Object', function (){
 
-        it('Проверка', function(){
-
-            var myDay = new Date(2011, 9, 30, 2, 30, 50, 320);
-
-
-            myDayString = myDay.toString();
-            expect(myDayString).toEqual('Sun Oct 30 2011 02:30:50 GMT+0300 (Саудовская Аравия, стандартное время)');
+        it ('.prototype', function (){
+            var human = {
+                    eats:true
+                };
             
-        });
+            function Man(name) {
+                this.name = name;
+            }
+            Man.prototype = human;
 
-        it('Установка года', function(){
+            var maxim = new Man('Maxim');
 
-            var myDay = new Date(2011, 9, 30, 2, 30, 50, 320);
-
-
-            myDay.setFullYear(1999);
-            expect(myDay.toString()).toEqual('Sat Oct 30 1999 02:30:50 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-            myDay.setFullYear(1999, 10, 5);
-            expect(myDay.toString()).toEqual('Fri Nov 05 1999 02:30:50 GMT+0300 (Саудовская Аравия, стандартное время)');
+            expect(maxim.eats).toBe(true);
+            expect(Object.getPrototypeOf(maxim)).toBe(human)
 
         });
+       
+        it ('constructor', function (){
+            function Man() {}
+            
+            expect(Object.getOwnPropertyNames(Man.prototype)).toEqual(['constructor']);
+            expect(Man.prototype.constructor === Man).toBeTruthy();
 
-        it('Установка месяца', function(){
+            function Man(name) {
+                this.name = name;
+            }
+            var maxim = new Man('Maxim');
+            var ivan = new maxim.constructor('Ivan');
+            
+            expect(maxim.name).toBe('Maxim');
+            expect(ivan.name).toBe('Ivan');
 
-            var myDay = new Date(2011, 9, 30, 2, 30, 50, 320);
+            Man.prototype = {
+                eats:true
+            }
 
+            expect(Man.prototype.constructor === Man).toBeFalsy();
+            
+            Man.prototype = {
+                eats:true,
+                constructor: Man
+            }
+            expect(Man.prototype.constructor === Man).toBeTruthy();
 
-            myDay.setMonth(5);
-            expect(myDay.toString()).toEqual('Thu Jun 30 2011 02:30:50 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-            myDay.setMonth(5, 15);
-            expect(myDay.toString()).toEqual('Wed Jun 15 2011 02:30:50 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-        });
-
-        it('Установка даты', function(){
-
-            var myDay = new Date(2011, 9, 30, 2, 30, 50, 320);
-            myDay.setDate(5);
-            expect(myDay.toString()).toEqual('Wed Oct 05 2011 02:30:50 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-        });
-
-        it('Установка часа', function(){
-
-            var myDay = new Date(2011, 9, 30, 2, 30, 50, 320);
-            myDay.setHours(6);
-            expect(myDay.toString()).toEqual('Sun Oct 30 2011 06:30:50 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-            myDay.setHours(6, 40, 15);
-            expect(myDay.toString()).toEqual('Sun Oct 30 2011 06:40:15 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-
-        });
-
-        it('Установка минут', function(){
-
-            var myDay = new Date(2011, 9, 30, 2, 30, 50, 320);
-            myDay.setMinutes(25);
-            expect(myDay.toString()).toEqual('Sun Oct 30 2011 02:25:50 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-            myDay.setMinutes(25, 20, 20);
-            expect(myDay.toString()).toEqual('Sun Oct 30 2011 02:25:20 GMT+0300 (Саудовская Аравия, стандартное время)');
-
+            expect(maxim.running).toBeUndefined();
+            Man.prototype.running = true;
+            maxim = new Man('Maxim');
+            expect(maxim.running).toBeDefined();
 
         });
-
-        it('Установка секунд', function(){
-
-            var myDay = new Date(2011, 9, 30, 2, 30, 50, 320);
-            myDay.setSeconds(5);
-            expect(myDay.toString()).toEqual('Sun Oct 30 2011 02:30:05 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-
-        });
-
-        it('Установка всей даты по миллисекундам', function(){
-
-            var myDay = new Date(2011, 9, 30, 2, 30, 50, 320);
-            myDay.setDate(5);
-            expect(myDay.toString()).toEqual('Wed Oct 05 2011 02:30:50 GMT+0300 (Саудовская Аравия, стандартное время)');
-
-        });
-
     });
 
-    it('Автоисправление даты', function(){
+    describe('built-in classes', function (){
 
-        var day = new Date(2015, 2, 30);
-        expect(day.toString()).toEqual('Mon Mar 30 2015 00:00:00 GMT+0300 (Саудовская Аравия, стандартное время)');
+        it ('{}', function (){
+            var obj = {};
 
-        day.setDate(day.getDate() + 2);
-        expect(day.toString()).toEqual('Wed Apr 01 2015 00:00:00 GMT+0300 (Саудовская Аравия, стандартное время)');
+            expect(obj.__proto__).toBeDefined();
+            expect(obj.__proto__.constructor).toBe(Object);
+
+            expect(obj.toString == Object.prototype.toString).toBeTruthy();
+            expect(obj.__proto__ == Object.prototype).toBeTruthy();
+            expect(obj.__proto__.__proto__).toBeNull()
+
+        });
+
+        it ('classes', function (){
+            var arr = [1,2,3];
+
+            expect(arr.__proto__ == Array.prototype).toBeTruthy();
+            expect(Array.prototype.__proto__ == Object.prototype).toBeTruthy();
+            expect(arr.toString()).toBe('1,2,3');
+
+            var date = new Date();
+            expect(date.__proto__ == Date.prototype).toBeTruthy();
+            expect(Date.prototype.__proto__ == Object.prototype).toBeTruthy();
+
+        });
+
+        it ('call and apply in prototype', function (){
+
+            function some(){
+                return [].join.call(arguments, "-")
+            }
+
+            expect(some('a','b','c')).toBe('a-b-c');
+
+            function some2(){
+                return Array.prototype.join.call(arguments, "-") 
+            }
+            expect(some2('1','2','3')).toBe('1-2-3');
+        });
+
+        it ('Primitives', function (){
+            var str = 'text',
+                num = 5,
+                bool = true;
+
+            expect(str.__proto__).toBe(String.prototype);
+            expect(num.__proto__).toBe(Number.prototype);
+            expect(bool.__proto__).toBe(Boolean.prototype);
+        });
+
+        it ('Changing the built-in prototypes', function () {
+            String.prototype.repeat = function(times) {
+                return new Array(times + 1).join(this);
+              };
+            var str = 'o';
+
+            expect(str.repeat(3)).toBe('ooo')
+              
+        });
+
+        it ('Own classes on prototype', function () {
+
+            function Car(model){
+                this.model =  model;
+                this.speed = 0;
+            }
+
+            Car.prototype.going = function (speed) {
+                this.speed += speed;
+            }
+
+            Car.prototype.stop = function() {
+                this.speed = 0;
+            }
+
+            var ford = new Car('Ford');
+            expect(ford.speed).toBe(0);
+
+            ford.going(50);
+            expect(ford.speed).toBe(50);
+
+            ford.stop();
+            expect(ford.speed).toBe(0);
+
+        });
+    });
+    describe('Inheritance of classes', function () {
+        it ('Inheritance', function () {
+            function Vehicle(model){
+                this.model =  model;
+                this.speed = 0;
+            }
+            Vehicle.prototype.going = function (speed) {
+                this.speed += speed;
+            }
+            Vehicle.prototype.stop = function() {
+                this.speed = 0;
+            }
+
+            function Ship(model){
+                this.model = model;
+                this.speed = 0;
+            }
+            Ship.prototype.swimming = function (){
+                this.swimming = true;
+            }
+
+            var boat = new Ship('Pobeda');
+            expect(Ship.prototype.stop).toBeUndefined();
+
+            Ship.prototype = Object.create(Vehicle.prototype);
+
+            Ship.prototype.constructor = Ship;
+            expect(Ship.prototype.stop).toBeDefined();
+        });
+
+        it ('Override', function () {
+            function Vehicle(model){
+                this.model =  model;
+                this.speed = 0;
+            }
+            Vehicle.prototype.going = function (speed) {
+                this.speed += speed;
+            }
+            Vehicle.prototype.stop = function() {
+                this.speed = 0;
+            }
+
+            function Ship(model){
+                this.model = model;
+                this.speed = 0;
+            }
+            Ship.prototype.swimming = function (){
+                this.speed++;
+            }
+            Ship.prototype.going = function (speed) {
+                this.speed += speed;
+                this.swimming();
+            }
+
+            expect(Vehicle.prototype.going !== Ship.prototype.going).toBeTruthy()
+        });
+
+        it ('call parents metod', function(){
+            function Vehicle(model){
+                this.model =  model;
+                this.speed = 0;
+            }
+            Vehicle.prototype.going = function (speed) {
+                this.speed += speed;
+            }
+            Vehicle.prototype.stop = function() {
+                this.speed = 0;
+            }
+
+            function ShipAmphibian(model){
+                Vehicle.apply(this, arguments);
+            }
+            ShipAmphibian.prototype.swimming = function (){
+                this.speed++;
+            }
+            ShipAmphibian.prototype.going = function (speed) {
+                Vehicle.prototype.going.apply(this, arguments);
+                this.swimming();
+            }
+
+            var amphibian = new ShipAmphibian('Beda');
+            amphibian.going(50);
+
+            expect(amphibian.speed).toBe(51)
+        });
     });
 
-    
+    describe('instanceOf', function () {
+        it ('instanceOf algorithm', function () {
+            function Car() {}
+
+            var ford = new Car();
+            var arr = []
+
+            expect(ford instanceof Car).toBeTruthy();
+            expect(arr instanceof Array).toBeTruthy();
+            expect(arr instanceof Object).toBeTruthy();
+        });
+    });
 });
-

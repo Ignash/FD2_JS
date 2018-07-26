485cbd8b6696dab73fb15a2c3e9a505ab385aa0c
Maksim Ihnashevich <maksimignashevich@gmail.com>
2018-07-26 10:49:52 +0300

    Drap&drop

--- main.js
+++ main.js
@@ -1,22 +1,63 @@
-showMenu(menu, 100, 10);
 
-function showMenu(element, x, y) {
-    let classList = element.classList,
-        style = element.style,
-        hideClass = 'hide',
-        pxUnit = 'px';
+const element = document.getElementById('circle'),
+      objElement = {};
 
-    // style.left = x + pxUnit;
-    // style.top = y + pxUnit;
-    
-    document.addEventListener('contextmenu', (event) => {
-        event.preventDefault();
+element.addEventListener ('mousedown', event => {
 
-        style.left = (x + event.pageX) + pxUnit;
-        style.top = (y + event.pageY) + pxUnit;
-        classList.remove(hideClass);
-    })
+    const biasX = element.getBoundingClientRect().left, 
+          biasY = element.getBoundingClientRect().top;
 
-    document.addEventListener('click', () => classList.add(hideClass))
-}
+    objElement.shiftY = event.pageY - biasY;
+    objElement.shiftX = event.pageX - biasX;
+    objElement.pressX = event.pageX - objElement.shiftX;
+    objElement.pressY = event.pageY - objElement.shiftY;
+    objElement.width = event.target.clientWidth;
+    objElement.height = event.target.clientHeight;
+
+    element.style.position = 'absolute';
+    document.body.appendChild(element);
+    element.style.zIndex = 1000; 
+
+    document.onmousemove = event => {
+        element.style.left = event.pageX - objElement.shiftX + 'px';
+        element.style.top = event.pageY - objElement.shiftY + 'px';
+    };
+
+    element.addEventListener('mouseup', event => {
+
+        document.onmousemove = null;
+
+        const CoordDropElementX = [(objElement.shiftX + 1)*(-1), objElement.width - objElement.shiftX + 1],
+        CoordDropElementY =[(objElement.shiftY + 1)*(-1), objElement.height - objElement.shiftY + 1];
+
+        let elemUp, 
+            elemDrop,
+            arrayTest = [];
+        
+        CoordDropElementX.forEach(x => {
+            CoordDropElementY.forEach(y => {
+                elemUp = document.elementFromPoint(event.clientX + x, event.clientY + y);
+                elemDrop = elemUp.closest('.droppable');
+ 
+                if (elemDrop) {
+                    arrayTest.push(true);  
+                }else {
+                    arrayTest.push(false);
+                }
+            })
+        });
+
+        if (arrayTest.every(item => item === true)) {
+            elemDrop.appendChild(element)
+        } else {
+            element.style.left = objElement.pressX + 'px';
+            element.style.top = objElement.pressY + 'px';
+        }
+    });
+});
+
+element.ondragstart = function() {
+  return false;
+};
+
 

523ed64524ab513c82811026966185ad1132dc3a
Maksim Ihnashevich <maksimignashevich@gmail.com>
2018-07-30 11:05:43 +0300

    name on canvas

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const canvas = document.getElementById('name'),
     ctx = canvas.getContext('2d');
 
 
-    ctx.lineWidth = 10;
-    ctx.lineCap = 'round';
-    ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
+ctx.lineWidth = 10;
 
 ctx.strokeStyle = '#09f';
 ctx.beginPath();
@@ -22,7 +22,6 @@ ctx.beginPath();
 ctx.moveTo(200, 150);
 ctx.lineTo(250, 50);
 ctx.lineTo(300, 150);
-
 ctx.moveTo(220, 110);
 ctx.lineTo(280, 110);
 ctx.stroke();
@@ -38,6 +37,7 @@ ctx.moveTo(350, 100);
 ctx.lineTo(420, 50);
 ctx.stroke();
 
+
 ctx.strokeStyle = 'red';
 ctx.beginPath();
 ctx.moveTo(450, 100);
@@ -47,8 +47,8 @@ ctx.arcTo(530,50,530,80, 30);
 ctx.moveTo(450, 100);
 ctx.arcTo(450,150,480,150, 30, false);
 ctx.arcTo(530,150,530,120, 30, false);
-
 ctx.stroke();
+
 
 ctx.strokeStyle = 'green';
 ctx.beginPath();
@@ -56,8 +56,8 @@ ctx.moveTo(580, 50);
 ctx.lineTo(580, 150);
 ctx.lineTo(660, 50);
 ctx.lineTo(660, 150);
-
 ctx.stroke();
+
 
 ctx.strokeStyle = '#09f';
 ctx.beginPath();
@@ -67,7 +67,3 @@ ctx.lineTo(760, 100);
 ctx.lineTo(810, 50);
 ctx.lineTo(810, 150);
 ctx.stroke();
-
-
-// ctx.lineTo(150, 50);
-// ctx.lineTo(150, 150);
